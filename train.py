79efbb1e5539cf383c9b7bf7c0fb689a794e2578
Ubuntu <hseto@h-seto.xk1mhvx2j2dujpyxrvcozr4yfb.bx.internal.cloudapp.net>
2018-06-02 07:06:06 +0000

    プロットコードを追加．

--- train.py
+++ train.py
@@ -17,57 +17,7 @@ import chainer.datasets as t
 
 from chainer.datasets import cifar
 
-
-class VGG(chainer.Chain):
-
-    def __init__(self, n_class=10):
-        super(VGG, self).__init__()
-        with self.init_scope():
-            self.conv1_1 = L.Convolution2D(None, 64, 3, pad=1)
-            self.bn1_1 = L.BatchNormalization(64)
-            self.conv1_2 = L.Convolution2D(64, 64, 3, pad=1)
-            self.bn1_2 = L.BatchNormalization(64)
-
-            self.conv2_1 = L.Convolution2D(64, 128, 3, pad=1)
-            self.bn2_1 = L.BatchNormalization(128)
-            self.conv2_2 = L.Convolution2D(128, 128, 3, pad=1)
-            self.bn2_2 = L.BatchNormalization(128)
-
-            self.conv3_1 = L.Convolution2D(128, 256, 3, pad=1)
-            self.bn3_1 = L.BatchNormalization(256)
-            self.conv3_2 = L.Convolution2D(256, 256, 3, pad=1)
-            self.bn3_2 = L.BatchNormalization(256)
-            self.conv3_3 = L.Convolution2D(256, 256, 3, pad=1)
-            self.bn3_3 = L.BatchNormalization(256)
-            self.conv3_4 = L.Convolution2D(256, 256, 3, pad=1)
-            self.bn3_4 = L.BatchNormalization(256)
-
-            self.fc4 = L.Convolution2D(256, 1024, 1, pad=0)
-            self.fc5 = L.Convolution2D(1024, 1024, 1, pad=0)
-            self.fc6 = L.Linear(None, n_class)
-
-    def __call__(self, x):
-        h = F.relu(self.bn1_1(self.conv1_1(x)))
-        h = F.relu(self.bn1_2(self.conv1_2(h)))
-        h = F.max_pooling_2d(h, 2, 2)
-        h = F.dropout(h, ratio=0.25)
-
-        h = F.relu(self.bn2_1(self.conv2_1(h)))
-        h = F.relu(self.bn2_2(self.conv2_2(h)))
-        h = F.max_pooling_2d(h, 2, 2)
-        h = F.dropout(h, ratio=0.25)
-
-        h = F.relu(self.bn3_1(self.conv3_1(h)))
-        h = F.relu(self.bn3_2(self.conv3_2(h)))
-        h = F.relu(self.bn3_3(self.conv3_3(h)))
-        h = F.relu(self.bn3_4(self.conv3_4(h)))
-        h = F.max_pooling_2d(h, 2, 2)
-        h = F.dropout(h, ratio=0.25)
-
-        h = F.dropout(F.relu(self.fc4(h)), ratio=0.5)
-        h = F.dropout(F.relu(self.fc5(h)), ratio=0.5)
-        h = self.fc6(h)
-        return h
+from model_builder import Builder
 
 
 if __name__ == "__main__":
@@ -90,8 +40,9 @@ if __name__ == "__main__":
 
     train_iter = iterators.SerialIterator(train, batchsize)
     test_iter = iterators.SerialIterator(test, batchsize, False, False)
-
-    model = VGG()
+    
+    builder = Builder(model_type="VGG")
+    model = builder.build()
 
     max_epoch = d["max_epoch"]
     model = L.Classifier(model)
@@ -109,7 +60,7 @@ if __name__ == "__main__":
     elif opt_str == "adam":
             optimizer = optimizers.Adam(alpha=lr)
 
-    base_dir = os.path.join("./", opt_str)
+    base_dir = os.path.join("./log", opt_str)
     optimizer.setup(model)
 
     updater = training.StandardUpdater(train_iter, optimizer, device=gpu_id)
